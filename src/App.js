403c07baf73c4860d2c9178ee72602b40b1c10d6
Gregoire <info@greeg.nl>
2022-05-25 14:56:49 +0200

    Updated homepage

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          My awesome GitHub page running using react!
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
